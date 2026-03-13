e4d1fde4fb53ea4fce8b99e382c961e3784f103a
IsLeony <isavmg21@gmail.com>
2026-03-13 11:49:48 -0300

    voltan 4.0

--- ecommerce-vue/src/App.vue
+++ ecommerce-vue/src/App.vue
@@ -22,10 +22,10 @@
         </div>
       </header>
 
-      <main class="grid grid-cols-1 gap-6 xl:grid-cols-3">
-        <section class="xl:col-span-2">
+      <main class="flex flex-col gap-6 lg:flex-row lg:items-start">
+        <section class="lg:w-2/3 xl:w-3/4">
           <h2 class="mb-4 text-xl font-extrabold">Produtos</h2>
-          <div class="grid grid-cols-1 gap-6 md:grid-cols-3 lg:grid-cols-4">
+          <div class="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3">
             <ProductCard
               v-for="product in products"
               :key="product.id"
@@ -35,7 +35,7 @@
           </div>
         </section>
 
-        <aside>
+        <aside class="lg:w-1/3 xl:w-1/4 lg:sticky lg:top-6 self-start">
           <Card class="rounded-2xl shadow-lg">
             <template #title>
               <div class="flex items-center justify-between">
